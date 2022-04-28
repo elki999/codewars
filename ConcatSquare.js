b7f009a6dedec67b4b1894609968f2f3a0453b94
dkiewit19 <dkiewit19@yahoo.com>
2022-04-28 17:50:35 -0400

    upload code

---
+++ ConcatSquare.js
@@ -0,0 +1,5 @@
+//In this kata, you are asked to square every digit of a number and concatenate them.
+
+function squareDigits(num){
+    return Number(num.toString().split('').map(num=>num**2).join(''))
+  }
