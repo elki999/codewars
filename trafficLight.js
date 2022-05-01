411cb2a13c3559a26158ac8d5612bef9bfb52107
dkiewit19 <dkiewit19@yahoo.com>
2022-05-01 17:05:58 -0400

    upload code

---
+++ trafficLight.js
@@ -0,0 +1,8 @@
+//Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
+
+function updateLight(current) {
+    if (current === "green") { return "yellow"}
+    else if (current === "yellow") {return "red"}
+    else {return "green"}
+  
+  }
