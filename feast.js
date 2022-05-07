052f569b8835c1ec0181e0a03377ceebed3e5112
dkiewit19 <dkiewit19@yahoo.com>
2022-05-07 19:51:20 -0400

    upload

---
+++ feast.js
@@ -0,0 +1,5 @@
+function feast(beast, dish) {
+    if (beast.startsWith() === dish.startsWith() && beast.charAt(beast.length-1) === dish.charAt(dish.length-1)) {return true}
+    else {return false}
+  
+  }
