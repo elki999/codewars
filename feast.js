93492fb8fb8c5381947bd0dbdabb935629c1d811
dkiewit19 <dkiewit19@yahoo.com>
2022-05-09 20:45:26 -0400

    upload

--- feast.js
+++ feast.js
@@ -1,5 +1,7 @@
 function feast(beast, dish) {
-    if (beast.startsWith() === dish.startsWith() && beast.charAt(beast.length-1) === dish.charAt(dish.length-1)) {return true}
+    if (beast.charAt(0) === dish.charAt(0) && beast.charAt(beast.length-1) === dish.charAt(dish.length-1)) {return true}
     else {return false}
   
   }
+
+  //Write a function feast that takes the animal's name and dish as arguments and returns true or false to indicate whether the beast is allowed to bring the dish to the feast.
