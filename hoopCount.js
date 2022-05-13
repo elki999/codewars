8369409461d8da9638b3cb5f9959cc4c9712e81d
dkiewit19 <dkiewit19@yahoo.com>
2022-05-13 15:14:32 -0400

    upload;

---
+++ hoopCount.js
@@ -0,0 +1,3 @@
+//Write a program where Alex can input (n) how many times the hoop goes round and it will return him an encouraging message :)
+
+const hoopCount = (n) => n<10 ? 'Keep at it until you get it' : 'Great, now move on to tricks'
