05ff356318642d664f5a5d30393609d9292a6c79
dkiewit19 <dkiewit19@yahoo.com>
2022-05-13 14:52:26 -0400

    upload

---
+++ reverse.js
@@ -0,0 +1,5 @@
+//You're at the zoo... all the meerkats look weird. Something has gone terribly wrong - someone has gone and switched their heads and tails around!
+
+function fixTheMeerkat(arr) {
+    return arr.reverse()
+   }
