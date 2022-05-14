1949fab4c3dc1b8acc785b8e59f8c41895907091
dkiewit19 <dkiewit19@yahoo.com>
2022-05-14 19:06:36 -0400

    upload

---
+++ Welcome.js
@@ -0,0 +1,27 @@
+//Write a 'welcome' function that takes a parameter 'language' (always a string), and returns a greeting - if you have it in your database. It should default to English if the language is not in the database, or in the event of an invalid input.
+
+function greet(language) {
+	const welcome = [
+    {
+    english: 'Welcome',
+czech: 'Vitejte',
+danish: 'Velkomst',
+dutch: 'Welkom',
+estonian: 'Tere tulemast',
+finnish: 'Tervetuloa',
+flemish: 'Welgekomen',
+french: 'Bienvenue',
+german: 'Willkommen',
+irish: 'Failte',
+italian: 'Benvenuto',
+latvian: 'Gaidits',
+lithuanian: 'Laukiamas',
+polish: 'Witamy',
+spanish: 'Bienvenido',
+swedish: 'Valkommen',
+welsh: 'Croeso'
+
+    }
+  ]
+  return welcome[0][language] === undefined ? 'Welcome' : welcome[0][language]
+}
