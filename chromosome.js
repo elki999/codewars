601e9ef58cf7701b51415f0898c6570dea676c63
dkiewit19 <dkiewit19@gmail.com>
2022-06-04 18:42:41 -0400

    upload

---
+++ chromosome.js
@@ -0,0 +1,4 @@
+//If the sperm contains the X chromosome, return "Congratulations! You're going to have a daughter."; If the sperm contains the Y chromosome, return "Congratulations! You're going to have a son.";
+
+let chromosomeCheck = sperm => sperm === 'XX' ? "Congratulations! You're going to have a daughter." :
+"Congratulations! You're going to have a son."
