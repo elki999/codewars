48f13d70dc221b7c4c05c68159e9acc998ab6aba
elki999 <dkiewit19@gmail.com>
2022-06-16 18:30:37 -0400

    upload

---
+++ FindtheDivisors.js
@@ -0,0 +1,8 @@
+function divisors(integer) {
+    let arr = []
+    for (let i=2; i<integer; i++) {
+      if (integer % i === 0) { arr.push(i)}
+    }
+      return (arr.length!=0)? arr : integer + " is prime"
+  
+  };
